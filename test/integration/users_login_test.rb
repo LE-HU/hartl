1dd7878bb59d1ddda5c8caeaf73c6f2c4a419b02
LE-HU <wojciechlech1337@gmail.com>
2020-07-30 16:29:41 +0200

    implement remember_token & remember_digest, remember me checkbox yet to come

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -27,7 +27,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid information, logout" do
+  test "login with valid information, followed by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email,
                                          password: "password" } }
@@ -41,6 +41,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,
