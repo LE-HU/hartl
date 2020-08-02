5a97ebbd55c2a0df90248cc4531eed115ee47a66
LE-HU <wojciechlech1337@gmail.com>
2020-08-03 00:43:34 +0200

    Finish user edit, update, index, and destroy actions, add admin functionality

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_select "title", full_title("Contact")
   end
+
+  test "additional layout links while logged in" do
+    log_in_as(users(:michael))
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", users_path
+    assert_select "a[class=dropdown-toggle]"
+  end
 end
