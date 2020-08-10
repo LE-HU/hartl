0181aa7c6f1816d1d1a0e1a2103e901e4b5ee421
LE-HU <wojciechlech1337@gmail.com>
2020-08-10 13:03:57 +0200

    implement ajax follow/unfollow functionality with buttons

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
@@ -16,4 +20,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
