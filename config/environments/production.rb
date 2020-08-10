22adbe5316c0b87644168b7553341459fa395d03
LE-HU <wojciechlech1337@gmail.com>
2020-08-10 13:46:20 +0200

    replace sendgrid with mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,27 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = "warm-lake-31697.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
+  # MAILGUN
   ActionMailer::Base.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => "587",
+    :port => ENV["MAILGUN_SMTP_PORT"],
+    :address => ENV["MAILGUN_SMTP_SERVER"],
+    :user_name => ENV["MAILGUN_SMTP_LOGIN"],
+    :password => ENV["MAILGUN_SMTP_PASSWORD"],
+    :domain => "yourapp.heroku.com",
     :authentication => :plain,
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
-    :domain => "heroku.com",
-    :enable_starttls_auto => true,
   }
+  ActionMailer::Base.delivery_method = :smtp
+
+  # SENDGRID FAILED IMPLEMENTATION
+  # ActionMailer::Base.smtp_settings = {
+  #   :address => "smtp.sendgrid.net",
+  #   :port => "587",
+  #   :authentication => :plain,
+  #   :user_name => ENV["SENDGRID_USERNAME"],
+  #   :password => ENV["SENDGRID_PASSWORD"],
+  #   :domain => "heroku.com",
+  #   :enable_starttls_auto => true,
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
